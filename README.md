bca380e7b097f51b170f2d9d176642242febd9c0
trondarild <trondarild@gmail.com>
2023-09-11 16:08:39 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ikaros_module_template
+# Ikaros Module Template
+
+Instructions:
+
+1. copy contents into Ikaros/Source/UserModules
+1. run: sh mkmodule.sh <name>
---
+++ mkmodule.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# usage: sh mkmodule.sh modulename
+
+MODULENAME="$1"
+ORGNAME="ModuleTemplate"
+## make new dir with MODULENAME
+mkdir $MODULENAME
+## copy everything from MyModule
+cp -R $ORGNAME/ $MODULENAME/
+cd $MODULENAME
+ls
+## rename all files
+for file in $(find . -name "*$ORGNAME*" -type f -maxdepth 2)
+    do
+        echo '    change' "$file" 'to' `echo "$file" | sed s/$ORGNAME/$MODULENAME/g`
+        mv "$file" `echo "$file" | sed s/$ORGNAME/$MODULENAME/g`
+    done
+ls
+## search and replace in files
+sed -e "s/$ORGNAME/$MODULENAME/; s/~$ORGNAME/~$MODULENAME/; s/\&$ORGNAME/\&$MODULENAME/; s:/$ORGNAME:/$MODULENAME:g;" -i "" *.*
+cd Examples/
+ls
+sed -e "s/$ORGNAME/$MODULENAME/; s/~$ORGNAME/~$MODULENAME/; s/\&$ORGNAME/\&$MODULENAME/; s:/$ORGNAME:/$MODULENAME:g;" -i "" *.*
+#cd ../..
+#sed -i "" -e '3i\'$'\n'"add_subdirectory($MODULENAME)" CMakeLists.txt
